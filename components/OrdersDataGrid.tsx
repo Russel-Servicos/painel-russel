83482d0041cd42612a21a6ec9fc28cee72976c74
Arthur Fusco <fuscoarthur1@gmail.com>
2022-10-04 12:46:42 -0300

    feat: componente OrdersDataGrid
    Este componente é a tabela de pedidos do ecommerce

---
+++ components/OrdersDataGrid.tsx
@@ -0,0 +1,163 @@
+import { Box, styled, Typography } from "@mui/material";
+import {
+  DataGrid,
+  DataGridProps,
+  GridColDef,
+  GridRenderCellParams,
+} from "@mui/x-data-grid";
+import Link from "next/link";
+import getPaymentMethod from "../services/getPaymentMethod";
+import { LightButton } from "./Button";
+import StatusChip from "./StatusChip";
+
+const RusselDataGrid = styled(DataGrid)<DataGridProps>(({ theme }) => ({
+  width: "100%",
+  background: theme.palette.neutral?.[2],
+  border: `1px solid ${theme.palette.neutral?.[4]}`,
+  "& .MuiDataGrid-cell": {
+    textAlign: "center",
+    padding: "12px 8px",
+    color: theme.palette.neutral?.[9],
+    borderBottom: `1px solid ${theme.palette.neutral?.[4]}`,
+    whiteSpace: "normal !important",
+    ".clamp": {
+      wordWrap: "break-word",
+      display: "-webkit-box",
+      WebkitLineClamp: "3",
+      WebkitBoxOrient: "vertical",
+      height: "auto",
+      width: "100%",
+      overflow: "hidden",
+    },
+  },
+  "& .MuiDataGrid-columnSeparator": { visibility: "hidden" },
+  "& .MuiDataGrid-columnHeaders": {
+    color: theme.palette.neutral?.[7],
+    background: theme.palette.neutral?.[3],
+    borderBottom: `1px solid ${theme.palette.neutral?.[4]}`,
+  },
+  "& .MuiDataGrid-footerContainer": {
+    borderTop: `1px solid ${theme.palette.neutral?.[4]}`,
+  },
+}));
+
+const columns: GridColDef[] = [
+  {
+    field: "id",
+    headerName: "CÓDIGO",
+    flex: 0.6,
+    headerAlign: "center",
+    align: "center",
+    renderCell: ({ value }) => (
+      <Typography variant="paragraph" className="clamp">
+        {value}
+      </Typography>
+    ),
+  },
+  {
+    field: "date",
+    headerName: "DATA",
+    flex: 0.6,
+    align: "center",
+    headerAlign: "center",
+    renderCell: (props: GridRenderCellParams<Date>) => {
+      const { value } = props;
+      return (
+        value && (
+          <div style={{ textAlign: "center" }}>
+            <Typography variant="paragraph" className="clamp">
+              {value?.getDate()}/{value?.getMonth() + 1}/{value?.getFullYear()}
+              <br />
+              às {value?.getHours()}:{value?.getMinutes()}
+            </Typography>
+          </div>
+        )
+      );
+    },
+  },
+  {
+    field: "client",
+    headerName: "CLIENTE",
+    flex: 0.8,
+    align: "center",
+    headerAlign: "center",
+  },
+  {
+    field: "payment",
+    headerName: "PAGAMENTO",
+    align: "center",
+    headerAlign: "center",
+    flex: 0.7,
+    renderCell: ({ value }) => {
+      return (
+        <Typography variant="paragraph" className="clamp">
+          {getPaymentMethod(value)}
+        </Typography>
+      );
+    },
+  },
+  {
+    field: "status",
+    headerName: "SITUAÇÃO",
+    flex: 1.3,
+    headerAlign: "center",
+    align: "center",
+    renderCell: ({ value }) => <StatusChip status={value} />,
+  },
+  {
+    field: "total",
+    headerName: "VALOR TOTAL",
+    align: "center",
+    headerAlign: "center",
+    flex: 0.9,
+    renderCell: ({ value }) => (
+      <Typography variant="paragraph">R${value}</Typography>
+    ),
+  },
+  {
+    field: "details",
+    headerName: "",
+    sortable: false,
+    disableColumnMenu: true,
+    align: "center",
+    flex: 1,
+    renderCell: ({ id }) => (
+      <Link href={`pedidos/${id}`}>
+        <a>
+          <LightButton>VER DETALHES</LightButton>
+        </a>
+      </Link>
+    ),
+  },
+];
+
+export interface OrdersDataGridProps {
+  rows: Array<{
+    id: string;
+    date: Date;
+    client: string;
+    payment: number;
+    status: string;
+    total: number;
+  }>;
+}
+
+const OrdersDataGrid = ({ rows }: OrdersDataGridProps) => {
+  return (
+    <Box display={"flex"} width={"100%"}>
+      <div style={{ flexGrow: 1 }}>
+        <RusselDataGrid
+          autoHeight
+          columns={columns}
+          rows={rows}
+          checkboxSelection
+          pageSize={10}
+          rowHeight={96}
+          rowsPerPageOptions={[10, 20, 30]}
+        />
+      </div>
+    </Box>
+  );
+};
+
+export default OrdersDataGrid;
